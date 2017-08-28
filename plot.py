2cc85c9878638606d42997dcafe3ace9df5850ab
Kevin Black <kevin3.black@gmail.com>
2017-08-27 23:30:21 -0500

    Add multi-subplot and line support

--- plot.py
+++ plot.py
@@ -5,22 +5,23 @@ import numpy as np
 
 class _CallablePlotter:
     """Private class used as the process callback to circumvent the fact that methods are not serializable"""
-    def __init__(self, n, titles):
-        self.lines = []
-        self.xdata, self.ydata = [[] for _ in range(n)], [[] for _ in range(n)]
-        self.fig, self.axes = plt.subplots(n, sharex=True)
+    def __init__(self, subplots, titles):
+        self.lines = [[] for _ in subplots]
+        self.xdata, self.ydata = [[[] for _ in range(m)] for m in subplots], [[[] for _ in range(m)] for m in subplots]
+        self.fig, self.axes = plt.subplots(len(subplots), sharex=True)
         self.fig.canvas.draw()
-        for ax, title in zip(self.axes, titles):
-            self.lines.append(ax.plot([], [])[0])
-            ax.set_title(title)
+        for i in range(len(subplots)):
+            self.axes[i].set_title(titles[i])
+            for _ in range(subplots[i]):
+                self.lines[i].append(self.axes[i].plot([], [])[0])
 
     def __call__(self, pipe):
         while True:
             if pipe.poll():
-                data = pipe.recv()
-                self.xdata[data[2]].append(data[0])
-                self.ydata[data[2]].append(data[1])
-                self.lines[data[2]].set_data(np.array(self.xdata[data[2]]), np.array(self.ydata[data[2]]))
+                x, y, n, m = pipe.recv()
+                self.xdata[n][m].append(x)
+                self.ydata[n][m].append(y)
+                self.lines[n][m].set_data(np.array(self.xdata[n][m]), np.array(self.ydata[n][m]))
                 for ax in self.axes:
                     ax.relim()
                     ax.autoscale_view()
@@ -42,12 +43,17 @@ class Plotter:
         If the main thread is terminated with a KeyboardInterrupt (Ctrl+C), the plot window will stay open, allowing
         one to explore and save the plot if desired.
     """
-    def __init__(self, n, *titles):
+    def __init__(self, subplots, *titles):
+        """
+        :param subplots: list of integers, the length of which will be the number of subplots, with each integer
+        representing the number of lines in the corresponding subplot
+        :param titles: list of titles for the subplots, must be same length as subplots
+        """
         self.receive, self.send = mp.Pipe(duplex=False)
         # Separate UI thread
-        self.process = mp.Process(target=_CallablePlotter(n, titles), args=(self.receive,))
+        self.process = mp.Process(target=_CallablePlotter(subplots, titles), args=(self.receive,))
         self.process.start()
 
-    def plot(self, x, y, n):
-        """Adds the point (x, y) to the nth subplot without blocking"""
-        self.send.send((x, y, n))
+    def plot(self, x, y, n, m):
+        """Adds the point (x, y) to the mth line on the nth subplot without blocking"""
+        self.send.send((x, y, n, m))
